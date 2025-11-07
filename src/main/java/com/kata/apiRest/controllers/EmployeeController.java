b065638ca3e4c0b4e85d73d19fbdc69005e40b84
Sandra Fdez <sandrafdezavila@gmail.com>
2025-11-07 14:06:17 +0100

    FIX: Small corrections

--- src/main/java/com/kata/apiRest/controllers/EmployeeController.java
+++ src/main/java/com/kata/apiRest/controllers/EmployeeController.java
@@ -1,15 +1,13 @@
 package com.kata.apiRest.controllers;
-
 import com.kata.apiRest.model.Employee;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/api/employee")
 public class EmployeeController {
 
     @GetMapping()
-    @ResponseStatus(HttpStatus.OK)
     public String getEmployee(){
         return "Return the employee details";
     }
@@ -21,19 +19,16 @@ public class EmployeeController {
     }
 
     @PutMapping()
-    @ResponseStatus(HttpStatus.OK)
     public String updateEmployee(@RequestBody Employee employee){
         return "Update employee details";
     }
 
     @DeleteMapping()
-    @ResponseStatus(HttpStatus.OK)
     public String deleteEmployee(@RequestBody Employee employee){
         return "Delete an employee";
     }
 
     @PatchMapping()
-    @ResponseStatus(HttpStatus.OK)
     public String patchEmployee(@RequestBody Employee employee){
         return "Update a specific detail of an employee";
     }
